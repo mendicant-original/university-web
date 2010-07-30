d688d55b1fe1e4493d2bdcac92260790a59a0bba
Jordan Byron <jordan@ducksoupsoftware.com>
2010-07-30 16:26:23 -0400

    Fix error in model namespace

--- app/controllers/chat/messages_controller.rb
+++ app/controllers/chat/messages_controller.rb
@@ -2,10 +2,10 @@ class Chat::MessagesController < ApplicationController
   def create
     message = JSON.parse(params[:message])
     
-    channel = ChatChannel.find_or_create_by_name(message["channel"])
-    handle  = ChatHandle.find_or_create_by_name(message["handle"])
+    channel = Chat::Channel.find_or_create_by_name(message["channel"])
+    handle  = Chat::Handle.find_or_create_by_name(message["handle"])
     
-    chat_message = ChatMessage.create( :handle_id   => handle.id,
+    chat_message = Chat::Message.create( :handle_id   => handle.id,
                                        :channel_id  => channel.id,
                                        :body        => message["body"],
                                        :recorded_at => message["recorded_at"])
