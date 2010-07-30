50e187af568a348269f18f2b8d8b5b3aa264a1a0
Jordan Byron <jordan@ducksoupsoftware.com>
2010-07-30 16:18:46 -0400

    Added json parse and message create to messages controller

--- app/controllers/chat/messages_controller.rb
+++ app/controllers/chat/messages_controller.rb
@@ -1,5 +1,15 @@
 class Chat::MessagesController < ApplicationController
   def create
-    raise params.inspect
+    message = JSON.parse(params[:message])
+    
+    channel = ChatChannel.find_or_create_by_name(message["channel"])
+    handle  = ChatHandle.find_or_create_by_name(message["handle"])
+    
+    chat_message = ChatMessage.create( :handle_id   => handle.id,
+                                       :channel_id  => channel.id,
+                                       :body        => message["body"],
+                                       :recorded_at => message["recorded_at"])
+                                       
+    render :json => chat_message.to_json
   end
 end
