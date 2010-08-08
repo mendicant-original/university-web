641cfcd07621a1bc469049627d98b812114d616d
Gregory Brown <gregory.t.brown@gmail.com>
2010-08-08 15:24:30 -0400

    Update migrations

--- db/migrate/20100801180547_create_users.rb
+++ db/migrate/20100801180547_create_users.rb
@@ -3,6 +3,7 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.database_authenticatable
       t.timestamps
+      t.boolean :requires_password_change, :default => true
     end
   end
 
