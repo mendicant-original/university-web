9083068b2f14d2b191056ea7e90013215d31ea16
Jordan Byron <jordan@ducksoupsoftware.com>
2010-08-12 12:51:46 -0400

    Only scroll to bottom if the scroll bar was currently at the bottom

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function isScrollBottom() {
+  var documentHeight = $(document).height();
+  var scrollPosition = $(window).height() + $(window).scrollTop();
+  return (documentHeight == scrollPosition);
+}
