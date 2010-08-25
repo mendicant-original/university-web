a775a68526a1ceaadb57c8ce6422a358b5aea50e
Jordan Byron <jordan@ducksoupsoftware.com>
2010-08-25 10:31:27 -0400

    Add student accociations to course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,7 @@
 class Course < ActiveRecord::Base
+  has_many :student_memberships
+  has_many :students, :through => :student_memberships
+  
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
