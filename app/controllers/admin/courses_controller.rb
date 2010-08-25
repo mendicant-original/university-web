e18ef978d213345a11db38b95a51467baf66bf8c
Jordan Byron <jordan@ducksoupsoftware.com>
2010-08-25 10:31:57 -0400

    Add course admin controller

---
+++ app/controllers/admin/courses_controller.rb
@@ -0,0 +1,43 @@
+class Admin::CoursesController < Admin::Base
+  before_filter :find_course, :only => [:show, :edit, :update, :destroy]
+  
+  def index
+    @courses = Course.all
+  end
+  
+  def new
+    @course = Course.new
+  end
+  
+  def create
+    @course = Course.new(params[:course])
+    
+    if @course.save 
+      redirect_to admin_users_path
+    else
+      render :action => :new
+    end
+  end
+  
+  def update
+    if @course.update_attributes(params[:course])
+      redirect_to admin_users_path
+    else
+      render :action => :edit
+    end
+  end
+  
+  def destroy
+    @course.destroy
+    
+    flash[:notice] = @course.errors.full_messages.join(",")
+    
+    redirect_to admin_users_path
+  end
+  
+  private
+  
+  def find_course
+    @course = Course.find(params[:id])
+  end
+end
---
+++ test/unit/helpers/admin/courses_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class Admin::CoursesHelperTest < ActionView::TestCase
+end
