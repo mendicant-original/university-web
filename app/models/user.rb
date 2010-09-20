e2369aead19aa3fc361dbe3fc3616d4036c33e16
Jordan Byron <jordan@ducksoupsoftware.com>
2010-09-20 17:11:46 -0400

    Add gravatar icon to dashboard

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,4 +41,10 @@ class User < ActiveRecord::Base
       ""
     end
   end
+  
+  def gravatar_url(size=40)
+    hash = Digest::MD5.hexdigest(email.downcase)
+    
+    "http://www.gravatar.com/avatar/#{hash}?s=#{size}"
+  end
 end
