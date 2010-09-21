c6652aaa947d9e20abeb0c9caffafef4c3f48f2c
Jordan Byron <jordan@ducksoupsoftware.com>
2010-09-21 18:17:23 -0400

    Add recent irc messages to dashboard

--- app/models/chat/channel.rb
+++ app/models/chat/channel.rb
@@ -5,7 +5,7 @@ class Chat::Channel < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_presence_of   :name
   
-  def recent(number_of_messages=5)
+  def recent(number_of_messages=2)
     messages.order("recorded_at DESC").limit(number_of_messages).reverse
   end
   
