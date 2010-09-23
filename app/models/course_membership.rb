eced12c32adec49f0a3a598f50259d8925d0af6b
Jordan Byron <jordan@ducksoupsoftware.com>
2010-09-23 11:12:01 -0400

    Adding a user to a course should create assignment submissions. Removing them from a course should remove their assignment submissions

--- app/models/course_membership.rb
+++ app/models/course_membership.rb
@@ -1,7 +1,25 @@
 class CourseMembership < ActiveRecord::Base
+  before_destroy :destory_assignment_submissions
+  after_create   :create_assignment_submissions
+  
   belongs_to :student, :class_name => "User", :foreign_key => "user_id"
   belongs_to :course
   
   validates_presence_of :course_id, :user_id
   validates_uniqueness_of :course_id, :scope => [:user_id]
+  
+  private
+  
+  def destory_assignment_submissions
+    student.assignment_submissions.includes(:assignment).
+      where(["assignments.course_id = ?", self.course_id]).each do |sub|
+        sub.delete
+      end
+  end
+  
+  def create_assignment_submissions
+    course.assignments.each do |assignment|
+      assignment.submission_for(student)
+    end
+  end
 end
