3a9a9245db4d11bcbf3709dd1a52a9a6bff89e1d
Jordan Byron <jordan@ducksoupsoftware.com>
2010-10-07 14:58:54 -0400

    Add user mailer and review emails

--- config/application.rb
+++ config/application.rb
@@ -47,5 +47,7 @@ module University
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    
+    config.action_mailer.default_url_options = { :host => "university.rubymendicant.com" }
   end
 end
