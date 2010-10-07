3698b29dcf02259e61fdc1f28179a4c44d3be485
Jordan Byron <jordan@ducksoupsoftware.com>
2010-10-07 17:21:52 -0400

    Submission status should be updated when a review is created

--- app/models/assignment/review.rb
+++ app/models/assignment/review.rb
@@ -1,9 +1,21 @@
 class Assignment::Review < ActiveRecord::Base
+  after_create :update_submission_status
+  
   belongs_to :submission
   
   has_many :comments, :as => :commentable
   
   def course
     submission.assignment.course
+  end
+  
+  private
+  
+  def update_submission_status
+    unless submission.status.name == "Approved"
+      submission.status = SubmissionStatus.find_by_name('Submitted')
+    
+      submission.save
+    end
   end
 end
