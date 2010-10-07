1e928ad0b1527bfc0349a3beff3101adc72f854c
Jordan Byron <jordan@ducksoupsoftware.com>
2010-10-07 10:39:42 -0400

    Add instructors to courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,10 +1,17 @@
 class Course < ActiveRecord::Base
   has_many :course_memberships, :dependent => :destroy
   has_many :students, :through => :course_memberships
+  
+  has_many :course_instructor_associations
+  has_many :instructors, :through => :course_instructor_associations
+  
   has_many :assignments
   
   validates_presence_of :name
   validates_uniqueness_of :name
   
   accepts_nested_attributes_for :assignments
+  accepts_nested_attributes_for :course_instructor_associations,
+    :reject_if => proc { |attributes| attributes['instructor_id'].blank? },
+    :allow_destroy => true
 end
