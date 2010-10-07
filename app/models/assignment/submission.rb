e78cff7e032b21211596ea8e56d9e23b81172b6a
Jordan Byron <jordan@ducksoupsoftware.com>
2010-10-07 10:14:38 -0400

    Rename Assignment::SubmissionStatus to SubmissionStatus

--- app/models/assignment/submission.rb
+++ app/models/assignment/submission.rb
@@ -1,5 +1,5 @@
 class Assignment::Submission < ActiveRecord::Base
-  belongs_to :status, :class_name  => "Assignment::SubmissionStatus", 
+  belongs_to :status, :class_name  => "::SubmissionStatus", 
                       :foreign_key => "submission_status_id"
                       
   belongs_to :user
