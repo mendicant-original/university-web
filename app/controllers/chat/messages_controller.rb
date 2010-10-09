fe0bfe9e4e43190cfc9c88d902772bc837d8a08f
Jaakko Vallo <jaakko.vallo@skillnet.fi>
2010-10-09 23:22:22 +0300

    Full text search. Handles also "no messages" situations with or without search terms.

--- app/controllers/chat/messages_controller.rb
+++ app/controllers/chat/messages_controller.rb
@@ -21,6 +21,7 @@ class Chat::MessagesController < ApplicationController
 
     @messages = Chat::Message.includes(:channel).
                               where("chat_channels.name = ?", channel || "#rmu-general").
+                              where("chat_messages.body ilike '%' || ? || '%'", (params[:search].nil? ? "" : params[:search] )).
                               order("recorded_at DESC")
                               
     @messages = @messages.where(:topic_id => topic.id) if topic
