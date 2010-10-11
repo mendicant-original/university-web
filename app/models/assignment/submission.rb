65446a5fe1990b53319883d59a5448b55c352190
Jordan Byron <jordan@ducksoupsoftware.com>
2010-10-11 17:08:24 -0400

    Add dependent delete all to reviews

--- app/models/assignment/submission.rb
+++ app/models/assignment/submission.rb
@@ -5,7 +5,7 @@ class Assignment::Submission < ActiveRecord::Base
   belongs_to :user
   belongs_to :assignment
   
-  has_many :reviews
+  has_many :reviews, :dependent => :delete_all
   
   def review
     reviews.where(:closed => false).first
