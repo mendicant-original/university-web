a76e6fbc243964fc206f7c0f8de2eaf2fe7e9027
Jordan Byron <jordan@ducksoupsoftware.com>
2010-10-11 13:57:52 -0400

    Fix old assignment submission status tests

--- test/functional/admin/submission_statuses_controller_test.rb
+++ test/functional/admin/submission_statuses_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Assignment::SubmissionStatusTest < ActiveSupport::TestCase
+class Admin::SubmissionStatusesControllerTest < ActionController::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
--- test/unit/submission_status_test.rb
+++ test/unit/submission_status_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class Admin::Assignments::SubmissionStatusesControllerTest < ActionController::TestCase
+class SubmissionStatusTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
