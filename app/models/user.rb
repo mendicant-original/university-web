e2b3bb98a41fedf7da3857b71ff6d45eb822c2ef
Jordan Byron <jordan@ducksoupsoftware.com>
2010-10-15 10:46:13 -0400

    Clean up user model formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,26 +1,36 @@
 class User < ActiveRecord::Base
+  GITHUB_FORMAT = { 
+    :with        => /^(?!-)[a-z\d-]+/i,
+    :message     => "can only contain alphanumeric characters and dashes. 
+                     Cannot start with a dash",
+    :allow_blank => true 
+  }
+
+  TWITTER_FORMAT = { 
+    :with        => /^\w+$/, 
+    :message     => "can only contain letters, numbers or underscores.",
+    :allow_blank => true 
+  }
+  
   devise :database_authenticatable, :validatable
   
   validates :twitter_account_name, :length => { :maximum => 15 },
-                                   :format   => { :with => /^\w+$/, 
-                                                  :message => "can only contain letters, numbers or underscores.",
-                                                  :allow_blank => true }
+                                   :format => TWITTER_FORMAT
                                                   
-  validates :github_account_name, :length => { :maximum => 40 },
-                                  :format   => { :with => /^(?!-)[a-z\d-]+/i,
-                                                 :message => "can only contain alphanumeric characters and dashes. 
-                                                 Cannot start with a dash",
-                                                 :allow_blank => true }                  
-                      
+  validates :github_account_name,  :length => { :maximum => 40 },
+                                   :format => GITHUB_FORMAT 
+                                  
   validate :real_name_or_nick_name_required
   
   has_many :chat_channel_memberships, :class_name => "Chat::ChannelMembership"
-  has_many :chat_channels, :through => :chat_channel_memberships, :source => :channel, :class_name => "Chat::Channel"
+  has_many :chat_channels,            :through    => :chat_channel_memberships, 
+                                      :source     => :channel, 
+                                      :class_name => "Chat::Channel"
 
-  has_many :course_memberships, :dependent => :destroy
-  has_many :courses, :through => :course_memberships
+  has_many :course_memberships,       :dependent  => :destroy
+  has_many :courses,                  :through    => :course_memberships
   
-  has_many :assignment_submissions, :class_name => "Assignment::Submission"
+  has_many :assignment_submissions,   :class_name => "Assignment::Submission"
   
   has_many :course_instructor_associations, :foreign_key => "instructor_id"
   
@@ -71,6 +81,9 @@ class User < ActiveRecord::Base
   end
   
   def real_name_or_nick_name_required 
-    errors.add(:base, "You need to provide either a real name or a nick name") if real_name.blank? and nickname.blank? 
+    if real_name.blank? and nickname.blank?
+      errors.add(:base, 
+                 "You need to provide either a real name or a nick name")
+    end
   end
 end
