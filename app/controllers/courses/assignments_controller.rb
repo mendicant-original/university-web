612f73e71fa191a8ee992e13ad40c7725b6543fc
Jordan Byron <jordan@ducksoupsoftware.com>
2010-10-21 16:46:24 -0400

    Add submissions sidebar to assignment

--- app/controllers/courses/assignments_controller.rb
+++ app/controllers/courses/assignments_controller.rb
@@ -1,7 +1,9 @@
 class Courses::AssignmentsController < Courses::Base
     
   def show
-    @assignment = @course.assignments.find(params[:id])
+    @assignment  = @course.assignments.find(params[:id])
+    @submissions = @assignment.submissions.includes(:status).
+                    order("submission_statuses.sort_order").group_by(&:status)
   end
   
 end
