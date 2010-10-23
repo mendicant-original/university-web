f2c6e1841810f7af060d27bd13397f2941da4e28
James A. Rosen <james@zendesk.com>
2010-10-23 13:40:51 -0700

    added tests for Course#full? and #available_slots

--- test/unit/course_test.rb
+++ test/unit/course_test.rb
@@ -1,8 +1,22 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+
+  context 'a Course with two slots and one student' do
+    setup do
+      @subject = Course.create!(:name => 'A Course', :class_size_limit => 2)
+      user = User.create!(:nickname => 'Me', :email => 'anybody@example.org', :password => '123456', :password_confirmation => '123456')
+      @subject.students << user
+    end
+
+    should 'not be full' do
+      assert !@subject.full?
+    end
+
+    should 'have one available slot' do
+      assert_equal 1, @subject.available_slots
+    end
+
   end
+
 end
