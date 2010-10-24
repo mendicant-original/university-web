a9d8d54aaa3b4d9499f8d088d9076b0c1d84fbb6
James A. Rosen <james@zendesk.com>
2010-10-23 17:34:14 -0700

    Refactored Course#start_end  - Course#start_end now returns a Range or nil  - added CoursesHelper#course_dates, which translates    such a Range into the former text  - added tests for both

--- test/unit/course_test.rb
+++ test/unit/course_test.rb
@@ -15,7 +15,26 @@ class CourseTest < ActiveSupport::TestCase
     should 'have one available slot' do
       assert_equal 1, @subject.available_slots
     end
+  end
 
+  context 'Course#start_end' do
+    context 'for a course with no start date' do
+      setup do
+        @course = Factory(:course, :start_date => nil, :end_date => 3.weeks.from_now)
+      end
+      should 'be nil' do
+        assert_nil @course.start_end
+      end
+    end
+
+    context 'for a course with a start date and an end date' do
+      setup do
+        @course = Factory(:course, :start_date => 2.days.ago, :end_date => 3.weeks.from_now)
+      end
+      should 'be a range from its start date to its end date' do
+        assert_equal((@course.start_date..@course.end_date), @course.start_end)
+      end
+    end
   end
 
 end
