8c9a78891414cfeea32e1e140b5a0f5f0af87a59
Jordan Byron <jordan@ducksoupsoftware.com>
2011-01-23 14:20:13 -0500

    When a student or mentor comments on another students' submission, they should be notified of any future comments made on that submission

--- app/helpers/user_mailer_helper.rb
+++ app/helpers/user_mailer_helper.rb
@@ -1,7 +1,8 @@
 module UserMailerHelper
   
-  def to_from_submission(submission, exclude_emails)
+  def to_from_submission(submission, exclude_emails, include_commentors=false)
     to =  submission.assignment.course.instructors.map {|i| i.email }
+    to += submission.comments.map {|c| c.user.email } if include_commentors
     to << submission.user.email
     
     to.uniq - [exclude_emails]
