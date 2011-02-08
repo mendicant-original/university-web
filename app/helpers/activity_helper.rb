0deb66c3127ebe7dc86cd3be9dde9e6e170f9fe6
Jordan Byron <jordan@ducksoupsoftware.com>
2011-02-08 13:45:37 -0500

    Description updates should be rolled up when repeated

--- app/helpers/activity_helper.rb
+++ app/helpers/activity_helper.rb
@@ -1,9 +1,32 @@
 module ActivityHelper
   extend self
-
+  
   def context_snippet(context, size=75)
     return context if context.length <= size
 
     context[0..size] + "..."
+  end
+  
+  # Returns an array of [Activity, [Activity, ...]] grouped by consecutive 
+  # Activity objects for description updates
+  #
+  def group_by_description(activities)
+    grouped_activities = Array.new
+    
+    activities.each do |activity|
+      if activity.description[/description/]
+        if grouped_activities.last && 
+           grouped_activities.last[0].description[/description/]
+          
+          grouped_activities.last[1] << activity
+        else
+          grouped_activities << [activity, Array.new]
+        end
+      else
+        grouped_activities << [activity, Array.new]
+      end
+    end
+    
+    grouped_activities
   end
 end
