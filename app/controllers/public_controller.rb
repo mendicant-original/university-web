9cdd996309631cdd1446504f72e092cb60d6aaba
Jaime Iniesta <jaimeiniesta@gmail.com>
2011-03-19 22:13:29 +0100

    Put the static layout back in place in PublicController

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -2,14 +2,16 @@ class PublicController < ApplicationController
   skip_before_filter :authenticate_user!
   skip_before_filter :change_password_if_needed
   before_filter      :set_current_section
-  
+
+  layout 'static'
+
   def alumni
     @alumni  = User.order("alumni_number").select {|u| u.alumnus? }
   end
-  
+
   def changelog
     @announcements = Announcement.where(:public => true).order("created_at DESC")
-    
+
     respond_to do |format|
       format.html do
         @announcements = @announcements.paginate(:page => params[:page], :per_page => 10)
@@ -17,16 +19,16 @@ class PublicController < ApplicationController
       format.rss { render :layout => false }
     end
   end
-  
+
   def announcement
     @announcement = Announcement.find_by_slug(params[:slug])
-    
+
     if @announcement.nil? || !@announcement.public?
       render(:text => "This announcement doesn't exist")
       return
     end
   end
-  
+
   def set_current_section
     @current = params[:action]
   end
