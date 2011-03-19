e0567783e1cf01fde506531f544b780a7e9cf35e
Jordan Byron <jordan@ducksoupsoftware.com>
2011-03-19 13:15:15 -0400

    Update term model

--- app/models/term.rb
+++ app/models/term.rb
@@ -1,7 +1,3 @@
 class Term < ActiveRecord::Base
   has_many :courses
-  has_many :exams
-
-  has_many :waitlisted_students, :dependent => :destroy
-  has_many :students,            :through   => :waitlisted_students
 end
