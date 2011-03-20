ac00d11ea516af90378c6c40756703607292464a
Jaime Iniesta <jaimeiniesta@gmail.com>
2011-03-20 23:08:57 +0100

    Unify date format in terms factories and tests

--- test/factories/term_factories.rb
+++ test/factories/term_factories.rb
@@ -1,5 +1,5 @@
 Factory.define :term do |f|
   f.name "T1"
-  f.start_date "1/1/2011".to_date
-  f.end_date "31/3/2011".to_date
+  f.start_date "2011-1-1".to_date
+  f.end_date "2011-3-31".to_date
 end
