f657092edfdc901100b011de35572a2480939e31
Jaime Iniesta <jaimeiniesta@gmail.com>
2011-03-21 23:19:14 +0100

    alumni_path route with optional year and slug

--- config/routes.rb
+++ config/routes.rb
@@ -1,47 +1,47 @@
 University::Application.routes.draw do
   root :to => "dashboard#show"
-  
-  get "alumni(/:year(/:term))"    => 'public#alumni', :constraints => { :year => /\d{4}/ }
+
+  get "alumni(/:year(/:term))"    => 'public#alumni', :constraints => { :year => /\d{4}/ }, :as => 'alumni'
   get "alumni/recent" => 'public#recent_alumni'
 
   get "changelog" => 'public#changelog'
   get "changelog/:slug" => 'public#announcement'
-  
+
   resource :dashboard, :controller => "dashboard"
-  
+
   namespace :chat do
     resources :messages
   end
   get 'chat/discussions/:channel' => 'chat/messages#discussions', :as => 'chat_discussions'
   get 'chat/discussion/url'       => 'chat/messages#discussion_topic_url'
-  
+
   get "transcripts/:channel" => 'Chat::Messages#transcripts'
 
   devise_for :users, :controllers => { :sessions => "user_sessions" }
 
   resource :home, :controller => "home"
-  
+
   get "slugger" => "slugger#index", :as => "slugger"
-  
+
   resources :users do
     member do
       post :change_password
     end
   end
-  
+
   resources :courses do
     member do
       post :notes
       get  :directory
     end
-    
+
     resources :documents, :controller => "Courses::Documents"
-    
+
     resources :assignments, :controller => "Courses::Assignments" do
       member do
         post :notes
       end
-      
+
       resources :submissions, :controller => "Courses::Assignments::Submissions" do
         member do
           post :comment
@@ -50,12 +50,12 @@ University::Application.routes.draw do
       end
     end
   end
-  
+
   resources :documents
-  
+
   get  "terms/:id/registration" => 'terms#registration', :as => 'registration'
   post "terms/:id/registration" => 'terms#register'
-  
+
   namespace :admin do
     resources :users
     resources :courses do
@@ -72,14 +72,14 @@ University::Application.routes.draw do
         get :user_emails
       end
     end
-    
+
     namespace :chat do
       resources :channels
     end
   end
-  
+
   resources :comments
-  
+
   namespace :admissions do
     resources :submissions do
       member do
@@ -90,5 +90,5 @@ University::Application.routes.draw do
   end
 
   get "/admissions" => 'Admissions::Submissions#new'
-  
+
 end
