f54ebc3a85a14a551a39369e598330e76ae22f65
Jordan Byron <jordan@ducksoupsoftware.com>
2011-03-22 14:24:06 -0400

    Ensure comment text is present for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,6 +2,8 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true
   belongs_to :user
   has_many   :actions, :as => :actionable
+  
+  validates_presence_of :comment_text
 
   def on
     created_at.strftime("%m/%d/%Y %I:%M%p")
