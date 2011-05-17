21e520138a7ba54b53f72629eb33fc3f7cad43d2
Jordan Byron <jordan@ducksoupsoftware.com>
2011-05-17 11:50:23 -0400

    Activites should be deleted when actionable items are delete

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,9 @@
 class Comment < ActiveRecord::Base
   belongs_to :commentable, :polymorphic => true
   belongs_to :user
-  has_many   :actions, :as => :actionable
+  has_many   :activities,  :class_name => "Assignment::Activity",
+                           :dependent  => :delete_all,
+                           :as         => :actionable
   
   validates_presence_of :comment_text
 
