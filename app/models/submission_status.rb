a8b60f849b93faad21ac59d6892314dba0061b7f
Jordan Byron <jordan@ducksoupsoftware.com>
2011-05-18 10:54:01 -0400

    Default submission status should create a temporary status rather than returning nil

--- app/models/submission_status.rb
+++ app/models/submission_status.rb
@@ -1,7 +1,11 @@
 class SubmissionStatus < ActiveRecord::Base
 
   def self.default
-    order("sort_order").first
+    default = order("sort_order").first
+    default ||= SubmissionStatus.create(
+      :name => "Pending Review", :sort_order => 0, :hex_color => "BD2B37")
+      
+    default
   end
 
 end
