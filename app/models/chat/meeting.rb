011ba8117111cc2216d9f7cb682e0193c091371b
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-05-24 15:43:31 +0530

    Add Chat::Meeting#messages method.

--- app/models/chat/meeting.rb
+++ app/models/chat/meeting.rb
@@ -2,4 +2,9 @@ class Chat::Meeting < ActiveRecord::Base
   belongs_to :topic
 
   validates_presence_of :started_at, :ended_at, :topic
+
+  def messages
+    Chat::Message.where(:channel_id => topic.channel_id).
+      where("recorded_at >= ? AND recorded_at <= ?", started_at, ended_at)
+  end
 end
