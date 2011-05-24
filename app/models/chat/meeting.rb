a6aa8d39a4d1904ca51e0ed48886cbc5a36247a7
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-05-24 16:15:42 +0530

    Make ended_at optional in Chat::Meeting, and default it to DateTime.now if it's not set.

--- app/models/chat/meeting.rb
+++ app/models/chat/meeting.rb
@@ -1,9 +1,11 @@
 class Chat::Meeting < ActiveRecord::Base
   belongs_to :topic
 
-  validates_presence_of :started_at, :ended_at, :topic
+  validates_presence_of :started_at, :topic
 
   def messages
+    self.ended_at ||= DateTime.now
+
     Chat::Message.where(:channel_id => topic.channel_id).
       where("recorded_at >= ? AND recorded_at <= ?", started_at, ended_at)
   end
