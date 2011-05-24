f6519da090fcb3211d8b8096d6d829d4318a11be
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2011-05-25 04:04:33 +0530

    Properly format actions in IRC messages, fixes #30.

--- app/models/chat/message.rb
+++ app/models/chat/message.rb
@@ -2,4 +2,12 @@ class Chat::Message < ActiveRecord::Base
   belongs_to :channel
   belongs_to :handle
   belongs_to :topic
+
+  def action?
+    body.start_with?("\u0001ACTION")
+  end
+
+  def action
+    body.gsub(/^\u0001ACTION(.*)\u0001$/, '\1')
+  end
 end
