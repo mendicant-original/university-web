060116ba890ccf6eec97424d9df12b7262b55a08
Jordan Byron <jordan@ducksoupsoftware.com>
2011-05-25 17:12:20 -0400

    Minor formatting fixes

--- app/models/chat/message.rb
+++ app/models/chat/message.rb
@@ -1,14 +1,14 @@
-class Chat::Message < ActiveRecord::Base
+class Chat::Message < ActiveRecord::Base  
+  before_create :check_action
+  
   belongs_to :channel
   belongs_to :handle
   belongs_to :topic
 
-  before_create :check_action
-
   def check_action
     regex = /^\u0001ACTION(.*)\u0001$/ 
     if body =~ regex
-      self.body = body.match(regex).captures.first
+      self.body   = body.match(regex).captures.first
       self.action = true
     end
   end
