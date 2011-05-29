80269275511b965b445cd29b69a60e594003b8dc
Jordan Byron <jordan@ducksoupsoftware.com>
2011-05-29 18:50:54 -0400

    Secret token file shouldn't be tracked

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-Rails.application.config.secret_token = '8a25ecfa18c9cae57096909e6271f39386e5d4dacd7a896e847915d3bf09bcec630af6fa0e101578f9296dbe07ed9c1453955e6e6998a8cf229c901effa8a291'
