023f27dc11d093c8f4968ac4bce7170ec7d619cd
Jordan Byron <jordan@ducksoupsoftware.com>
2011-06-05 17:47:33 -0400

    Remove static google map

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,23 +24,23 @@ module ApplicationHelper
 
     return html.html_safe
   end
-  
+
   def user_button
     link_to(
-      [ current_user.name, 
-        image_tag("down_arrow.png", :class => "drop") 
+      [ current_user.name,
+        image_tag("down_arrow.png", :class => "drop")
       ].join.html_safe,
       '#'
     )
   end
-  
+
   def selected_arrow(section)
     return nil
     if defined?(@selected) && @selected == section
       image_tag "selected_arrow.png", :class => "selected"
     end
   end
-  
+
   def button_away(name, url, options = {})
     form_tag(url, :method => :get) do
       [
@@ -48,32 +48,5 @@ module ApplicationHelper
         submit_tag(name)
       ].join("\n").html_safe
     end
-  end
-
-  # Generates a map containing pins for all user locations. Uses Google Maps
-  # Static API (http://code.google.com/apis/maps/documentation/staticmaps).
-  #
-  # Options:
-  #   width,
-  #   height: the width / height in pixels for the image; default for both
-  #           is 512px
-  #
-  #   zoom_level: zoom level for the map; default is 0 ('world'). See
-  #               http://code.google.com/apis/maps/documentation/staticmaps/#Zoomlevels
-  #               for details.
-  def user_map_tag(options={})
-    width      = options[:width]      || 512
-    height     = options[:height]     || 512
-    zoom_level = options[:zoom_level] || 0
-
-    markers = User.locations.map { |loc| loc.join(',') }.join('%7C')
-
-    url = "http://maps.google.com/maps/api/staticmap?" +
-      "sensor=false" +
-      "&zoom=#{zoom_level}" +
-      "&size=#{width}x#{height}" +
-      "&markers=#{markers}"
-
-    image_tag(url, :alt => 'User Map')
   end
 end
