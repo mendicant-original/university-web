3759a19bb5c0fbad6cdf677bffee0b0750a1a263
Jordan Byron <jordan@ducksoupsoftware.com>
2011-06-06 15:18:28 -0400

    If a submission status is deleted, it should not be the end of the world

--- app/helpers/admissions/submissions_helper.rb
+++ app/helpers/admissions/submissions_helper.rb
@@ -1,11 +1,11 @@
 module Admissions::SubmissionsHelper
 
   def submission_zip_path(submission)
-    attachment_admissions_submission_path(submission.id, 
-      "#{submission.user.name.underscore}_#{submission.id}", 
+    attachment_admissions_submission_path(submission.id,
+      "#{submission.user.name.underscore}_#{submission.id}",
       :format => "zip")
   end
-  
+
   def link_to_submission_user(submission)
     if current_access_level.allows? :manage_users
       link_to submission.user.name, edit_admin_user_path(submission.user)
@@ -13,4 +13,13 @@ module Admissions::SubmissionsHelper
       link_to submission.user.name, admissions_submission_path(submission)
     end
   end
+
+  def submission_status_name(status)
+    status.try(:name) || 'Unknown'
+  end
+
+  def submission_status_hex_color(status)
+    status.try(:hex_color) || '333'
+  end
+
 end
