326401944f8d94737a7038c1c2e83fc7baf930fe
Solomon White <rubysolo@gmail.com>
2011-06-08 19:02:41 -0600

    use custom marker icon and shadow

--- public/javascripts/uw.maps.js
+++ public/javascripts/uw.maps.js
@@ -13,10 +13,22 @@ UW.Maps.init = function(points){
 
   var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 
+  var image = new google.maps.MarkerImage('images/marker.png',
+      new google.maps.Size(40, 40),
+      new google.maps.Point(0,0),
+      new google.maps.Point(15, 36));
+
+  var shadow = new google.maps.MarkerImage('images/marker-shadow.png',
+      new google.maps.Size(40, 40),
+      new google.maps.Point(0,0),
+      new google.maps.Point(15, 36));
+
   $(points).each(function(p, point){
     new google.maps.Marker({
-      position: new google.maps.LatLng(point[0],point[1]),
-      map: map
+      position:  new google.maps.LatLng(point[0],point[1]),
+      map:       map,
+      shadow:    shadow,
+      icon:      image
     });
   });
 }
