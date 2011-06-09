98c4bd3f4e360a8f7e7a62e248c40912e5a7d7f7
Jordan Byron <jordan@ducksoupsoftware.com>
2011-06-09 09:25:27 -0400

    Add test notifier to project

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'capybara/rails'
 require 'support/integration'
+require 'test_notifier/runner/test_unit'
 
 class ActiveSupport::TestCase
   # fixtures :all
