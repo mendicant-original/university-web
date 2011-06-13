2af1ddb69af30554163ff722352db1316abd3070
Chris Geihsler <chris@geihsler.net>
2011-06-12 23:08:27 -0400

    cleaned up grouping and now grouping by github commit

--- app/helpers/activity_helper.rb
+++ app/helpers/activity_helper.rb
@@ -11,22 +11,57 @@ module ActivityHelper
   # Activity objects for description updates
   #
   def group_by_description(activities)
-    grouped_activities = Array.new
-    
-    activities.each do |activity|
-      if activity.description[/description/]
-        if grouped_activities.last && 
-           grouped_activities.last[0].description[/description/]
-          
-          grouped_activities.last[1] << activity
-        else
-          grouped_activities << [activity, Array.new]
-        end
+    ActivityGrouper.new(activities).group_by_description
+  end
+
+  class ActivityGrouper
+
+    GROUPABLE_METHODS = [
+        :is_a_description_update?,
+        :is_a_github_commit?
+    ]
+
+    def initialize(activities)
+      @activities = activities
+    end
+
+    def group_by_description
+      @grouped_activities = []
+
+      @activities.each do |activity|
+        group(activity)
+      end
+
+      @grouped_activities
+    end
+
+    private
+
+    def group(activity)
+      if is_groupable?(activity)
+        add_to_current_group(activity)
       else
-        grouped_activities << [activity, Array.new]
+        create_new_group(activity)
       end
     end
-    
-    grouped_activities
+
+    def is_groupable?(activity)
+       GROUPABLE_METHODS.any? do |method|
+         activity.send(method) && current_group && current_group[0].send(method)
+       end
+    end
+
+    def create_new_group(activity)
+      @grouped_activities << [activity, Array.new]
+    end
+
+    def add_to_current_group(activity)
+      current_group[1] << activity
+    end
+
+    def current_group
+      @grouped_activities.last
+    end
+
   end
 end
