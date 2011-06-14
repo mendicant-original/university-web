b0e07c0d98a0b7cdb906f36127aa253dd443a435
Jordan Byron <jordan@ducksoupsoftware.com>
2011-06-14 10:59:07 -0400

    Add activity log tests

--- test/factories/activity_factories.rb
+++ test/factories/activity_factories.rb
@@ -1,4 +1,6 @@
+Factory.sequence(:activity_description) { |n| "made something #{n} times" }
+
 Factory.define :activity, :class => Assignment::Activity do |f|
   f.association :user
-  f.description "made something"
+  f.description { |_| Factory.next(:activity_description) }
 end
---
+++ test/integration/courses/activity_test.rb
@@ -0,0 +1,40 @@
+require 'test_helper'
+require 'mocha'
+
+module Courses
+  class ActivityTest < ActionDispatch::IntegrationTest
+
+    story "As a Student I want to view an activity" do
+
+      setup do
+        @user       = sign_user_in
+        @course     = Factory(:course)
+        @assignment = Factory(:assignment, :course => @course)
+        Factory(:course_membership, :user => @user, :course => @course)
+
+        @submission = @assignment.submission_for(@user)
+
+        5.times do
+          Factory(:activity, :actionable => @submission,
+            :submission => @submission, :assignment => @assignment)
+        end
+      end
+
+      scenario "view all activities" do
+        visit course_path(@course, :anchor => "activity")
+
+        assert_content "made something"
+      end
+
+      # This is currently broken in most web browsers but not reproducing here
+      # https://www.pivotaltracker.com/story/show/14551241
+      scenario "view an activity" do
+        visit course_path(@course, :anchor => "activity")
+
+        click_link(@course.activities.first.description)
+
+        assert_current_path edit_course_assignment_submission_path(@course, @assignment, @submission)
+      end
+    end
+  end
+end
