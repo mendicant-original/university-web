e963f008c93807396a5cd16fb007298c3cca85aa
Jordan Byron <jordan@ducksoupsoftware.com>
2011-07-07 15:50:55 -0400

    Add a little style to the reply to box

--- public/javascripts/uw.comments.js
+++ public/javascripts/uw.comments.js
@@ -8,20 +8,20 @@ UW.Comments.init = function(commentsPath){
     trigger('edit');
     e.preventDefault();
   });
-  
+
   $('.comment .content a').live('click', function(e){
     window.open(e.target.href);
     return false;
   });
-  
+
   $('.comment[data-editable=true]').each(function(i,el){
     id = $(el).attr('data-id');
-    
+
     $(el).children(".content").click(function(e){
       if (e.target.tagName != "A")
         $(e.target).trigger('edit');
     });
-  
+
     $(el).children(".content").editable(commentsPath + id, {
       type:      'textarea',
       method:    'PUT',
@@ -37,22 +37,22 @@ UW.Comments.init = function(commentsPath){
   });
 
   $('.comment a.reply').click(function(e){
-    var $reply_container = $("form.new_comment .in_reply_to"),
-        $comment         = $(this).closest('.comment'),
-        id               = $comment.attr('data-id'),
-        index            = $comment.attr('id');
+    var reply_container = $("form.new_comment #in_reply_to"),
+        comment         = $(this).closest('.comment'),
+        id              = comment.attr('data-id'),
+        index           = comment.attr('id');
 
-    $reply_container.find("input[type='hidden']").val(id);
-    $reply_container.find("a:first").attr('href', '#' + index)
-                                    .text('comment #' + index);
-    $reply_container.show();
+    reply_container.find("input[type='hidden']").val(id);
+    reply_container.find("a:first").attr('href', '#' + index)
+                                    .text('Comment #' + index);
+    reply_container.show();
     $("form.new_comment textarea").focus();
   });
 
-  $('form.new_comment .in_reply_to a.cancel_reply').click(function(e){
-    var $reply_container = $("form.new_comment .in_reply_to");
-    $reply_container.find("input[type='hidden']").val('');
-    $reply_container.fadeOut();
+  $('form.new_comment #in_reply_to a.cancel_reply').click(function(e){
+    var reply_container = $("form.new_comment #in_reply_to");
+    reply_container.find("input[type='hidden']").val('');
+    reply_container.fadeOut();
     return false;
   });
 }
