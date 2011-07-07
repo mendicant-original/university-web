15f350581a1781a7470bbc9ea645ff35f1aa4e40
Jordan Byron <jordan@ducksoupsoftware.com>
2011-07-07 15:37:56 -0400

    Change reply icon and update comment header

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,6 @@
 module CommentsHelper
 
   def link_to_comment(comment)
-    link_to "comment ##{comment.index}", "##{comment.index}"
+    link_to "#{comment.user.name}'s comment", "##{comment.index}"
   end
 end
