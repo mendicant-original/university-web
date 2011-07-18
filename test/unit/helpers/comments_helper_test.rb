216ac346d0f6c9b57ec12bf6f236cae6f9d28d77
Jordan Byron <jordan@ducksoupsoftware.com>
2011-07-18 11:17:58 -0400

    Fix comment helper test

--- test/unit/helpers/comments_helper_test.rb
+++ test/unit/helpers/comments_helper_test.rb
@@ -4,13 +4,13 @@ class CommentsHelperTest < ActionView::TestCase
   include CommentsHelper
 
   context "#link_to_comment" do
-    
+
     setup do
       @comment = Factory(:comment)
     end
 
     should 'show the number of comment' do
-      assert_match /comment #1/, link_to_comment(@comment)
+      assert_match /#{@comment.user.name}'s comment/, link_to_comment(@comment)
     end
 
     should 'have a anchor link to comment' do
