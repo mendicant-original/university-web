f05353fab04744dacbb63797b0051bb63c7522e6
Jordan Byron <jordan@ducksoupsoftware.com>
2011-07-25 18:12:53 -0400

    Make sure zip file names are valid

--- app/helpers/admissions/submissions_helper.rb
+++ app/helpers/admissions/submissions_helper.rb
@@ -2,7 +2,7 @@ module Admissions::SubmissionsHelper
 
   def submission_zip_path(submission)
     attachment_admissions_submission_path(submission.id,
-      "#{submission.user.name.underscore}_#{submission.id}",
+      "#{Slugger.generate(submission.user.name)}_#{submission.id}",
       :format => "zip")
   end
 
