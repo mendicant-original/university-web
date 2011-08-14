4602284fa96b1ffdeee52fd684fe7b76b3be3e02
Jordan Byron <jordan@ducksoupsoftware.com>
2011-08-14 15:54:20 -0400

    Show comment numbers

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,6 +1,6 @@
 module CommentsHelper
 
   def link_to_comment(comment)
-    link_to "#{comment.user.name}'s comment", "##{comment.index}"
+    link_to "#{comment.user.name}'s comment (##{comment.index})", "##{comment.index}"
   end
 end
