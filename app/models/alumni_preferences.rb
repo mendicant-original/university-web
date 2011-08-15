2aa38922905dc6599f5aab5ab8a55e21a980ae95
Jordan Byron <jordan@ducksoupsoftware.com>
2011-08-15 09:16:54 -0400

    Make show_on_public_site default to opt in

--- app/models/alumni_preferences.rb
+++ app/models/alumni_preferences.rb
@@ -4,9 +4,9 @@ class AlumniPreferences < ActiveRecord::Base
   private
 
   def set_defaults
-    self.show_on_public_site = false if show_on_public_site.nil?
-    self.show_twitter        = true  if show_twitter.nil?
-    self.show_github         = true  if show_github.nil?
-    self.show_real_name      = true  if show_real_name.nil?
+    self.show_on_public_site = true if show_on_public_site.nil?
+    self.show_twitter        = true if show_twitter.nil?
+    self.show_github         = true if show_github.nil?
+    self.show_real_name      = true if show_real_name.nil?
   end
 end
