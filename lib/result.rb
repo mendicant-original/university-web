e3c5ffa63219c8003b8e739b6dfecc4260fc57a3
Nerian <siotopo@gmail.com>
2011-09-27 14:57:03 +0200

    Load the results with Javascript

--- lib/result.rb
+++
@@ -1,8 +0,0 @@
-class Result
-  attr_accessor :description, :content, :link
-  def initialize(options)
-    @description = options[:description]
-    @content = options[:content]
-    @link = options[:link]
-  end
-end
