5d4b81f5713d86cf1e6e8cfb0cb37eb7fa81860c
Jordan Byron <jordan@ducksoupsoftware.com>
2011-11-14 08:36:43 -0500

    Fix slugger encoding issues

--- app/models/slugger.rb
+++ app/models/slugger.rb
@@ -1,11 +1,13 @@
+#encoding: UTF-8
+
 module Slugger
   extend self
-  
+
   def clean_smart_quotes(text)
-    text.gsub! "\342\200\230", "'"
-    text.gsub! "\342\200\231", "'"
-    text.gsub! "\342\200\234", '"'
-    text.gsub! "\342\200\235", '"'  
+    text.gsub! "\xe2\x80\x99", "'"
+    text.gsub! "\xe2\x80\x98", "'"
+    text.gsub! "\xe2\x80\x9c", '"'
+    text.gsub! "\xe2\x80\x9d", '"'
     text
   end
 
@@ -15,5 +17,5 @@ module Slugger
     slug_text.gsub!(/<[^>]+>/, '')
     slug_text.split(' ')[0..5].join('-').downcase
   end
-  
+
 end
