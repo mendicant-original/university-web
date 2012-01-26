1d5a3f7bf7aae42c3c1850f1c1b2ac2e43d56d99
Pablo A. Torres Navarrete <tn.pablo@gmail.com>
2012-01-26 18:00:20 -0500

    Finish integration tests for Course#search

--- test/integration/courses/search_test.rb
+++ test/integration/courses/search_test.rb
@@ -8,13 +8,16 @@ module Courses
         setup do
           tests_javascript
 
-          @course = Factory(:course, :description => 'lorem')
+          channel = Factory(:chat_channel, :name => "mendicant")
+          message = Factory(:chat_message, :channel => channel,
+                             :body => "chat message")
+          @course = Factory(:course,
+                            :description => 'lorem',
+                            :notes => 'ipsum',
+                            :channel => channel)
+
           @user = sign_user_in
           Factory(:course_membership, :user => @user, :course => @course)
-          2.times do |i|
-            user = Factory(:user, :nickname => "Student #{i+1}")
-            Factory(:course_membership, :user => user, :course => @course)
-          end                       
         end
             
         test "I don't get result if there is no match" do
@@ -32,31 +35,61 @@ module Courses
         end                                            
         
         test "I get results from the Notes" do
-          pending()
+          visit course_path(@course, :anchor => "search")
+          fill_in 'search', with: 'ipsum'
+          click_button('Search')
+          assert_content "Shared Notes"
         end                   
         
         context "# In its IRC" do
           test "I get results from the log" do
-            pending()
+            visit course_path(@course, :anchor => "search")
+            fill_in 'search', with: 'chat message'
+            click_button('Search')
+            assert_content "Irc Channel"
           end
         end
                 
         context "# In an Assigment" do
+          setup do
+            @assignment = Factory(:assignment, :course => @course,
+                                  :description => "dolor", :notes => "sit amet")
+          end
+
           test "I get results from the Description" do
-            pending()
+            visit course_path(@course, :anchor => "search")
+            fill_in 'search', with: 'dolor'
+            click_button('Search')
+            assert_content "Assignments"
           end        
           
           test "I get results from the notes" do
-            pending()
+            visit course_path(@course, :anchor => "search")
+            fill_in 'search', with: 'sit amet'
+            click_button('Search')
+            assert_content "Assignments"
           end                   
           
           context "# In a Submission" do
+            setup do
+              @submission = Factory(:submission, :assignment => @assignment,
+                                   :description => "test description")
+            end
+
             test "I get results form the description" do
-              pending()
+              visit course_path(@course, :anchor => "search")
+              fill_in 'search', with: 'test description'
+              click_button('Search')
+              assert_content "Submissions"
             end                                   
             
             test "I get results from the Comments" do
-              pending()
+              @comment = Factory(:comment, :commentable => @submission,
+                                 :comment_text => "comment text")
+              visit course_path(@course, :anchor => "search")
+              fill_in 'search', with: 'comment text'
+              click_button('Search')
+              assert_content "Submission Comments"
             end
           end
         end                                  
