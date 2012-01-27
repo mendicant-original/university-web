9592340ecc12e01feff701c526b7987b3799f75c
Jordan Byron <jordan@ducksoupsoftware.com>
2012-01-26 20:30:21 -0500

    Fix typo in tests

--- test/integration/courses/search_test.rb
+++ test/integration/courses/search_test.rb
@@ -3,7 +3,7 @@ require 'mocha'
 
 module Courses
   class SearchTest < ActionDispatch::IntegrationTest
-    context "As a user, I want to have full text-search capabilities" do            
+    context "As a user, I want to have full text-search capabilities" do
       context "# In a course" do
         setup do
           tests_javascript
@@ -19,28 +19,28 @@ module Courses
           @user = sign_user_in
           Factory(:course_membership, :user => @user, :course => @course)
         end
-            
+
         test "I don't get result if there is no match" do
           visit course_path(@course, :anchor => "search")
           fill_in 'search', with: 'blablabla'
           click_button('Search')
           assert_content 'Found 0 results'
         end
-        
+
         test "I get results from the course's description" do
           visit course_path(@course, :anchor => "search")
           fill_in 'search', with: 'lorem'
           click_button('Search')
           assert_content "Course's description"
-        end                                            
-        
+        end
+
         test "I get results from the Notes" do
           visit course_path(@course, :anchor => "search")
           fill_in 'search', with: 'ipsum'
           click_button('Search')
           assert_content "Shared Notes"
-        end                   
-        
+        end
+
         context "# In its IRC" do
           test "I get results from the log" do
             visit course_path(@course, :anchor => "search")
@@ -49,7 +49,7 @@ module Courses
             assert_content "Irc Channel"
           end
         end
-                
+
         context "# In an Assigment" do
           setup do
             @assignment = Factory(:assignment, :course => @course,
@@ -61,28 +61,28 @@ module Courses
             fill_in 'search', with: 'dolor'
             click_button('Search')
             assert_content "Assignments"
-          end        
-          
+          end
+
           test "I get results from the notes" do
             visit course_path(@course, :anchor => "search")
             fill_in 'search', with: 'sit amet'
             click_button('Search')
             assert_content "Assignments"
-          end                   
-          
+          end
+
           context "# In a Submission" do
             setup do
               @submission = Factory(:submission, :assignment => @assignment,
                                    :description => "test description")
             end
 
-            test "I get results form the description" do
+            test "I get results from the description" do
               visit course_path(@course, :anchor => "search")
               fill_in 'search', with: 'test description'
               click_button('Search')
               assert_content "Submissions"
-            end                                   
-            
+            end
+
             test "I get results from the Comments" do
               @comment = Factory(:comment, :commentable => @submission,
                                  :comment_text => "comment text")
@@ -92,8 +92,8 @@ module Courses
               assert_content "Submission Comments"
             end
           end
-        end                                  
-      end      
+        end
+      end
     end
   end
 end
